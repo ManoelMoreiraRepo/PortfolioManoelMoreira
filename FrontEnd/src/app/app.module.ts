9f7b3473c415b1a922684449477f3a08c5c15070
Manoel Moreira <mmnmoreira@gmail.com>
2022-12-26 21:53:37 -0300

    struture Angular

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { HySComponent } from './components/hy-s/hy-s.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LogoapComponent } from './components/logoap/logoap.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HySComponent,
+    FooterComponent,
+    LogoapComponent,
+    BannerComponent,
+    ProyectoComponent
   ],
   imports: [
     BrowserModule,
