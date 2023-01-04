871fd3c2cf103d543114bd8d9aa3a50671f98de2
Manoel Moreira <mmnmoreira@gmail.com>
2023-01-04 14:55:41 -0300

    Include JWT
    Inclusión en el BackEnd del Java Web Tolken

--- BackEnd/manoelmoreira/src/main/java/com/portfolio/manoelmoreira/Controller/PersonaController.java
+++ BackEnd/manoelmoreira/src/main/java/com/portfolio/manoelmoreira/Controller/PersonaController.java
@@ -5,6 +5,7 @@ import com.portfolio.manoelmoreira.Entity.Persona;
 import com.portfolio.manoelmoreira.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,13 +27,14 @@ public class PersonaController {
     return ipersonaService.getPersona(); 
     
     }
-    
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/personas/crear")
     public String createPersona(@RequestBody Persona persona){
     ipersonaService.savePersona(persona);
     return "La persona fue creada corectamente";
     }
-     
+    
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/personas/borrar/{id}")
     public String deletePersona(@PathVariable Long id){
     ipersonaService.deletePersona(id);
@@ -41,6 +43,7 @@ public class PersonaController {
 
     
     //URL:PUERTO//personas/editar/1? nombre & apellido & img
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/personas/editar/{id}")
     public Persona editPersona(@PathVariable Long id,
             @RequestParam("nombre") String nuevoNombre,
