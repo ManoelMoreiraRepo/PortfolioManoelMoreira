ae7917481d7df3c8b423e6b53c0b1c5fa3d634cf
Manoel Moreira <mmnmoreira@gmail.com>
2023-02-16 21:14:57 -0300

    Solucionado problema de edicion de educaciion

--- FrontEnd/src/app/components/educacion/editeducacion.component.ts
+++ FrontEnd/src/app/components/educacion/editeducacion.component.ts
@@ -1,5 +1,6 @@
+import { PlatformLocation } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, ChildrenOutletContexts, Router } from '@angular/router';
 import { Educacion } from 'src/app/model/educacion';
 import { EducacionService } from 'src/app/service/educacion.service';
 
@@ -9,34 +10,36 @@ import { EducacionService } from 'src/app/service/educacion.service';
   styleUrls: ['./editeducacion.component.css']
 })
 export class EditeducacionComponent implements OnInit {
-  educacion:Educacion=null
+  educacion: Educacion = null;
+  
   constructor(
     private educacionS: EducacionService,
-    private activatedRouter: ActivatedRoute,
+    private activatedRouter : ActivatedRoute,
     private router: Router
-  ){ }
+  ) { }
 
   ngOnInit(): void {
-    const id =this.activatedRouter.snapshot.params['id'];
-    this.educacionS.details(id).subscribe(
+    const id = this.activatedRouter.snapshot.params['id'];
+    this.educacionS.detail(id).subscribe(
       data =>{
         this.educacion = data;
       }, err =>{
-        alert('Error al modificar');
-        this.router.navigate(['']);
+         alert("Error al modificar");
+         this.router.navigate(['']);
       }
     )
   }
 
-  onUpdate():void {
-  const id = this.activatedRouter.snapshot.params['id'];
-  this.educacionS.update(id, this.educacion).subscribe(
-    data => {
-      this.router.navigate(['']);      
-    }, err =>{
-      alert("Error al modificar la educacion");
-      this.router.navigate(['']);
-    }
-  )
+  onUpdate(): void{
+    const id = this.activatedRouter.snapshot.params['id'];
+    this.educacionS.update(id, this.educacion).subscribe(
+      data => {
+        this.router.navigate(['']);
+      }, err => {
+        alert("Error al modificar la educacion");
+        this.router.navigate(['']);
+      }
+    )
+  }
 }
-}
+
