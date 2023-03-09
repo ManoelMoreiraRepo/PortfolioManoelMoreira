3b3abb9eb1fac694e8fee53925a541bfcb71b7fc
Manoel Moreira <mmnmoreira@gmail.com>
2023-03-09 00:29:09 -0300

    metodo up`date agregado a persona.service

--- FrontEnd/src/app/components/acerca-de/acerca-de.component.ts
+++ FrontEnd/src/app/components/acerca-de/acerca-de.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { persona } from 'src/app/model/persona.model';
 import { PersonaService } from 'src/app/service/persona.service';
+import { TokenService } from 'src/app/service/token.service';
 
 @Component({
   selector: 'app-acerca-de',
@@ -8,14 +9,26 @@ import { PersonaService } from 'src/app/service/persona.service';
   styleUrls: ['./acerca-de.component.css']
 })
 export class AcercaDeComponent implements OnInit{
-  persona: persona =new persona("","","");
+  persona: persona = null;
+  private tokenService: TokenService;
   
-  constructor(public personaService: PersonaService){
+  constructor(public personaService: PersonaService,
+    tokenService: TokenService){
 
   }
   
   ngOnInit(): void {
-    this.personaService.getpersona().subscribe(data => this.persona = data)
+  this.cargarPersonas();
+  if (this.tokenService.getToken()){
+    this.isLogged=true;
+  }else{
+    this.isLogged=false;
+  }  
   }
-
+  isLogged=false;
+  
+  cargarPersonas(){
+    this.personaService.detail(1).subscribe(data =>
+      {this.persona = data})
+  }
 }
