bb97fa5d68b77e266b177276febb4cfada94c2f8
Manoel Moreira <mmnmoreira@gmail.com>
2023-03-13 20:02:19 -0300

    Inclusion de Firebase y edicion de acrca de en el front

---
+++ FrontEnd/src/app/service/image.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImageService {
+
+  constructor() { }
+
+  public uploadImage($event: any) {
+    const file=$event.target.file[0]
+    console.log(file);
+     
+  }
+}
