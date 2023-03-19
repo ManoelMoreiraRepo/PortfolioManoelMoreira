9e131a447dc20035720216df7b67289be00eff1a
Manoel Moreira <mmnmoreira@gmail.com>
2023-03-19 01:20:51 -0300

    Solucionado problemas con imagenes en el frontend

--- FrontEnd/src/app/components/acerca-de/acerca-de.component.ts
+++ FrontEnd/src/app/components/acerca-de/acerca-de.component.ts
@@ -10,7 +10,7 @@ import { TokenService } from 'src/app/service/token.service';
 })
 export class AcercaDeComponent implements OnInit{
 /*persona: persona = new  persona("Manoel","Moreira","","");*/
-  persona: persona = null;
+persona: persona = null;
     
   constructor(public personaService: PersonaService,private tokenService: TokenService){ }
   isLogged=false;
--- FrontEnd/src/app/components/acerca-de/edit-acerca-de.component.ts
+++ FrontEnd/src/app/components/acerca-de/edit-acerca-de.component.ts
@@ -7,45 +7,50 @@ import { PersonaService } from 'src/app/service/persona.service';
 @Component({
   selector: 'app-edit-acerca-de',
   templateUrl: './edit-acerca-de.component.html',
-  styleUrls: ['./edit-acerca-de.component.css'],
+  styleUrls: ['./edit-acerca-de.component.css']
 })
 export class EditAcercaDeComponent implements OnInit {
   persona: persona = null;
+  
   constructor(
     private activatedRouter: ActivatedRoute,
     private personaService: PersonaService,
     private router: Router,
-    public imageService: ImageService) {}
+    public imageService: ImageService
+  ) { }
 
   ngOnInit(): void {
     const id = this.activatedRouter.snapshot.params['id'];
     this.personaService.detail(id).subscribe(
-      (data) => {
+      data => {
         this.persona = data;
-      },
-      (err) => {
-        alert('Error al modificar');
+      }, 
+      err => {
+        console.log(err);
+        alert("Error al modificar");
         this.router.navigate(['']);
       }
-    );
+    )
   }
 
   onUpdate(): void {
     const id = this.activatedRouter.snapshot.params['id'];
+    this.persona.img = this.imageService.url;
     this.personaService.update(id, this.persona).subscribe(
-      (data) => {
+      data => {
         this.router.navigate(['']);
-      },
-      (err) => {
-        alert('Error al modificar');
+      }, 
+      err => {
+        console.log(err);
+        alert("Error al actualizar la persona");
         this.router.navigate(['']);
       }
     );
   }
 
-  uploadImage($event: any) {
+  uploadImage($event:any){
     const id = this.activatedRouter.snapshot.params['id'];
-    const name= "perfil_" + id;
-    this.imageService.uploadImage($event, name)
+    const name = "perfil_" + id;
+    this.imageService.uploadImage($event, name);
   }
 }
